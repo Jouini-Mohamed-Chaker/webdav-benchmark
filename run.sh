51f53259306dd71509e2861d7874fb9ffcbfc0d2
Mohamed Chaker Jouini <JouiniMohamedChaker@proton.me>
2026-08-10 10:35:25 +0100

    move to apache instead of nginx for the webdav support

--- run.sh
+++ run.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-# run-bench.sh
+# run.sh - provisioning only.
+# Benchmarking is no longer run through Ansible; use benchmark.sh or
+# rclone_benchmark.sh directly from the client after this completes.
 export ANSIBLE_FORCE_COLOR=1
 export PY_COLORS=1
-ansible-playbook -i inventory.ini playbook.yml \
-  -e numa_node_client="${1:-0}" \
-  -e file_size_mb="${2:-2000}" \
-  -e parallel_file_mb="${3:-200}" \
-  -e repeats="${4:-3}"
+ansible-playbook -i inventory.ini playbook.yml
