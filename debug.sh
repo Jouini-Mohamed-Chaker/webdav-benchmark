ebb04cc75042a08d30eb2f74b6feb2292f25eab3
Jouini-Mohamed-Chaker <JouiniMohamedChaker@proton.me>
2026-08-24 00:30:44 +0100

    Tweaking debug script

--- debug.sh
+++ debug.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ssh root@192.168.95.2 'fio --name=disktest --filename=/data/webdav/fio_test --size=2G \
+ssh root@192.168.95.2 'fio --name=disktest --filename=/data/webdav/fio_test --size=200M \
   --rw=write --bs=1M --iodepth=8 --numjobs=8 --direct=0 --group_reporting'
 
 # ssh root@192.168.95.2 rm /data/webdav/fio_test
